20479d8ec3c639e000da601f546327caf35b08ab
podmar <marta.podziewska@gmail.com>
2023-01-03 13:57:02 +0100

    sort and archive files by extension

--- downloads-cleaner.sh
+++ downloads-cleaner.sh
@@ -3,14 +3,27 @@
 # Sort and archive files in the downloads folder
 # Author: podmar
 
-# direcotry_to_clean="$1"
-# cd $directory_to_clean
+# directory_to_clean="$1"
+# cd "$directory_to_clean"
 
 cd $HOME/Desktop/test_downloads
 
 directory_name="dir_archive_$(date +"%d-%m-%Y")"
 mkdir "$directory_name"
 
-find . -maxdepth 1 -mindepth 1 -type d -mtime +30 | xargs -I '{}' mv {} ./$directory_name
+find . -maxdepth 1 -mindepth 1 -type d -mtime +30 |
+xargs -I '{}' mv {} ./$directory_name
 
 echo "$directory_name folder created and items older than 30 days archived"
+
+# list all file extensions in the folder to a variable separated by spaces
+extension_list=$(find . -maxdepth 1 -type f -name "[^.]*.*" | sed "s|.*\.||" | sort -u | tr "\n" " ")
+
+# for each extension create a folder and move files older than 30 days inside
+for extension in $extension_list; do
+    dir_name=${extension}_archive_$(date +"%d-%m-%Y")
+    mkdir "$dir_name"
+    find . -maxdepth 1 -type f -name "*.$extension" -mtime +30 |
+    xargs -I '{}' mv {} ./"$dir_name"
+    echo "$extension files older than 30 days archived in $dir_name"
+done
