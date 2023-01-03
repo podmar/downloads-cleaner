80f0037de4434b2e4162b369da0abba1346d2c85
podmar <marta.podziewska@gmail.com>
2023-01-03 14:16:36 +0100

    changed  time limit to 14 days and applied

--- downloads-cleaner.sh
+++ downloads-cleaner.sh
@@ -1,29 +1,28 @@
 #!/usr/bin/env bash
 
-# Sort and archive files in the downloads folder
+# Sort and archive items in a selected folder
 # Author: podmar
 
-# directory_to_clean="$1"
-# cd "$directory_to_clean" || exit
+directory_to_clean="$1"
+cd "$directory_to_clean" || exit
 
-cd $HOME/Desktop/test_downloads || exit
-
+# move all folders older than 14 days to one archive directory
 directory_name="dir_archive_$(date +"%d-%m-%Y")"
 mkdir "$directory_name"
 
-find . -maxdepth 1 -mindepth 1 -type d -mtime +30 |
+find . -maxdepth 1 -mindepth 1 -type d -mtime +14 |
 xargs -I "{}" mv {} ./"$directory_name"
 
-echo "$directory_name folder created and items older than 30 days archived"
+echo "All folders older than 14 days archived in $directory_name"
 
 # list all file extensions in the folder to a variable separated by spaces
-extension_list=$(find . -maxdepth 1 -type f -name "[^.]*.*" | sed "s|.*\.||" | sort -u | tr "\n" " ")
+extension_list=$(find . -maxdepth 1 -type f -name "[^.]*.*" -mtime +14 | sed "s|.*\.||" | sort -u | tr "\n" " ")
 
-# for each extension create a folder and move files older than 30 days inside
+# for each extension create a folder and move files older than 14 days inside
 for extension in $extension_list; do
     dir_name=${extension}_archive_$(date +"%d-%m-%Y")
     mkdir "$dir_name"
-    find . -maxdepth 1 -type f -name "*.$extension" -mtime +30 |
+    find . -maxdepth 1 -type f -name "*.$extension" -mtime +14 |
     xargs -I '{}' mv {} ./"$dir_name"
-    echo "$extension files older than 30 days archived in $dir_name"
+    echo "$extension files older than 14 days archived in $dir_name"
 done
